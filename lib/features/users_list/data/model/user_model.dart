d00706a14fcdbe98c127d4752dccfdf263295d0b
mohamad hadi <87898758+mohamad-hadi-said@users.noreply.github.com>
2024-11-04 16:34:27 +0300

    add user model

---
+++ lib/features/users_list/data/model/user_model.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:rikaz_team/features/users_list/domain/entities/user.dart';
+
+class UserModel extends User {
+  UserModel(
+      {required super.id,
+      required super.email,
+      required super.firstName,
+      required super.lastName,
+      required super.avatar});
+
+     
+  UserModel copyWith({
+    int? id,
+    String? email,
+    String? firstName,
+    String? lastName,
+    String? avatar,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      avatar: avatar ?? this.avatar,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'email': email,
+      'first_name': firstName,
+      'last_name': lastName,
+      'avatar': avatar,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] as int,
+      email: map['email'] as String,
+      firstName: map['first_name'] as String,
+      lastName: map['last_name'] as String,
+      avatar: map['avatar'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  bool get stringify => true; 
+}
